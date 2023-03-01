2813ee3b712a5dac040356c16ab17c60e27d9a31
Faalw001 <faalw001@gmail.com>
2023-03-01 11:36:10 +0100

    Update Snacks method added

--- src/main/java/lia/practice/mongoflux/MongofluxApplication.java
+++ src/main/java/lia/practice/mongoflux/MongofluxApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MongofluxApplication {
-
+//testar pusha från test branch
 	public static void main(String[] args) {
 		SpringApplication.run(MongofluxApplication.class, args);
 	}
