d908cc5b4b86b770ca060d2dc23b9bb0f0c14e98
osho81 <ossi.hour@gmail.com>
2023-03-01 12:07:29 +0100

    comment main class o

--- src/main/java/lia/practice/mongoflux/MongofluxApplication.java
+++ src/main/java/lia/practice/mongoflux/MongofluxApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MongofluxApplication {
 //testar pusha från test branch
 //testar pusha från test branch 2
+//testar pusha från test branch 3 // Osama
 	public static void main(String[] args) {
 		SpringApplication.run(MongofluxApplication.class, args);
 	}
