4f01bd0665798155b37cd509fcd41efbf16783c1
osho81 <ossi.hour@gmail.com>
2023-03-04 11:16:20 +0100

    Add log4j logging example ins ervice class

--- src/main/java/lia/practice/mongoflux/service/SnackService.java
+++ src/main/java/lia/practice/mongoflux/service/SnackService.java
@@ -2,6 +2,8 @@ package lia.practice.mongoflux.service;
 
 import lia.practice.mongoflux.model.Snack;
 import lia.practice.mongoflux.repository.SnackRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,13 @@ import reactor.core.publisher.Mono;
 @Service
 public class SnackService {
 
+    private static final Logger logger = LogManager.getLogger(SnackService.class);
+
     @Autowired
     private SnackRepository snackRepository;
 
-
     public Flux<Snack> getAllSnacks() {
+        logger.info("Get all snacks");
         return snackRepository.findAll();
     }
 
