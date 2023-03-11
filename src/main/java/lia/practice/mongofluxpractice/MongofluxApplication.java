878a3eba09f90af89aabf4dbded4aead893bc0bd
osho81 <ossi.hour@gmail.com>
2023-03-11 12:37:01 +0100

    Test update without token

--- src/main/java/lia/practice/mongofluxpractice/MongofluxApplication.java
+++ src/main/java/lia/practice/mongofluxpractice/MongofluxApplication.java
@@ -3,6 +3,8 @@ package lia.practice.mongofluxpractice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Testing without credentials
+
 @SpringBootApplication
 public class MongofluxApplication {
 	public static void main(String[] args) {
